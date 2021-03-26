1fa8279b3c295b1300c7850ef6b3a6f4d77592e8
Jamie Dalan Cox <74336097+dalancox@users.noreply.github.com>
2021-03-26 12:55:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ window.addEventListener('load', ()=> {
             
             //grabbing the open weather api 
             //using proxy for local development
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e430828eaa2a466ac69e449d7b72800b`;
+            //const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e430828eaa2a466ac69e449d7b72800b`;
 
  
 
