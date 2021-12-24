5ba5cea2bbb9db177555e5ff911fb679e3d28716
dalancox <dalanjcox@gmail.com>
2021-12-23 23:30:58 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 window.addEventListener('load', ()=> {
-    let lon;
-    let lat;
 
     //grabbing html elements
 
@@ -10,93 +8,91 @@ window.addEventListener('load', ()=> {
     let displayIcon = document.querySelector(".icon");
     let degreeSection = document.querySelector(".degree-section");
     const convertDegrees = document.querySelector(".degree-section span");
+    const btnSub = document.getElementById('submit');
 
+    btnSub.addEventListener('click', getVal);
 
     // if statement get current postion of user
 
-    if(navigator.geolocation){
-        // navigator grabs user location and puts it into variables
-        navigator.geolocation.getCurrentPosition(position => {
-            lon = position.coords.longitude;
-            lat = position.coords.latitude;
-            
-            //grabbing the open weather api 
-            //using proxy for local development                       
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e430828eaa2a466ac69e449d7b72800b`;
-            console.log(api);
- 
+    function getVal() { 
+        const city = document.getElementById('city-name').value
 
-            //using fetch to get api from server
+        console.log(city)
 
-            fetch(api)
-                .then(data => {
-                    //putting data in a json format
-                    return data.json();
-                })
-                    .then(data => {
-                        //grapping the data and putting it into my variables
-                        console.log(data.timezone);
-                        let temp = data.main.temp;
-                        console.log(temp);
-                        let cityName = data.name;
-                        console.log(cityName);
-                        let description = data.weather[0].description;
-                        console.log(description);
-                        let country = data.sys.country;
-                        console.log(country);
-                        let icon = data.weather[0].icon;
-                        console.log(icon);
-
-                        const iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-
-                        function convertFahrenheit() {
-                            temp = (temp - 273.15) * 9/5 + 32;
-                            console.log(temp);
-                        }
+          //grabbing the open weather api 
+    //using proxy for local development                       //e430828eaa2a466ac69e449d7b72800b
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const api = `${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&appid=e430828eaa2a466ac69e449d7b72800b`;
+    console.log(api);
 
 
-                        //convert temp into fahrenheit by default
-                        convertFahrenheit();
+    //using fetch to get api from server
+     fetch(api)
+        .then(data => {
+            //putting data in a json format
+            return data.json();
+        })
+            .then(data => {
+                //grapping the data and putting it into my variables
+                console.log(data.timezone);
+                let temp = data.main.temp;
+                console.log(temp);
+                let cityName = data.name;
+                console.log(cityName);
+                let description = data.weather[0].description;
+                console.log(description);
+                let country = data.sys.country;
+                console.log(country);
+                let icon = data.weather[0].icon;
+                console.log(icon);
 
-                        //fomula for celsius 
-                        let celsius = (temp - 32) * 5/9;
+                const iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
 
-                        //event listener for changing from F to C
-                        degreeSection.addEventListener('click', ()=> {
-                            if(convertDegrees.textContent === 'F') {
-                                convertDegrees.textContent = 'C';
-                                temperature.textContent = Math.floor(celsius);
+                function convertFahrenheit() {
+                    temp = (temp - 273.15) * 9/5 + 32;
+                    console.log(temp);
+                }
 
-                            } else {
-                                convertDegrees.textContent = 'F';
-                                temperature.textContent = Math.floor(temp);
-                            
-                            }
 
-                        })
+                //convert temp into fahrenheit by default
+                convertFahrenheit();
 
-                        //assigning DOM elements to the API 
-                        weatherDescription.textContent = `${description}`;
+                //fomula for celsius 
+                let celsius = (temp - 32) * 5/9;
+
+                //event listener for changing from F to C
+                degreeSection.addEventListener('click', ()=> {
+                    if(convertDegrees.textContent === 'F') {
+                        convertDegrees.textContent = 'C';
+                        temperature.textContent = Math.floor(celsius);
+
+                    } else {
+                        convertDegrees.textContent = 'F';
                         temperature.textContent = Math.floor(temp);
-                        location.textContent = `${cityName}, ${country}`;
-                        displayIcon.src = iconUrl;
+                    
+                    }
 
-                        //grabbing local timezone of user
+                })
 
-                        timezone = data.timezone;
-                        var d = new Date((new Date().getTime())-25200*1000);
+                //assigning DOM elements to the API 
+                weatherDescription.textContent = `${description}`;
+                temperature.textContent = Math.floor(temp);
+                location.textContent = `${cityName}, ${country}`;
+                displayIcon.src = iconUrl;
 
-                        //d.toISOString();
+                //grabbing local timezone of user
 
-                        console.log(d);
-                    })
+                timezone = data.timezone;
+                var d = new Date((new Date().getTime())-25200*1000);
 
-        });
-    }else {
-        //error message if geolocation doesn't work
+                //d.toISOString();
 
+                console.log(d);
+            })
+                .catch(error => console.log(error));
     }
+    
+  
 });
 
 
